a5c14b3e4b7bd8aa761cc7c463bbd661b49f0cf7
Ward Lootens <wardl@mips.be>
2018-01-24 16:05:58 +0100

    Create vim directories in bootstrap file

--- bootstrap-nix.sh
+++ bootstrap-nix.sh
@@ -7,6 +7,10 @@ cd $HOME
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/.vim ~/.vim
 
+mkdir .vim/backup_files
+mkdir .vim/swap_files
+mkdir .vim/undo_files
+
 ## GIT
 ln -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/git/.gitignore_global ~/.gitignore_global
