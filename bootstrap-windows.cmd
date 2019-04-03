ceeaa1f8cc13badd0410c9d97ae7789f27231a98
Ward Lootens <wardl@mips.be>
2019-04-03 16:53:33 +0200

    Enhance bootstrap script for windows

--- bootstrap-windows.cmd
+++ bootstrap-windows.cmd
@@ -1,13 +1,44 @@
-setx HOME %USERPROFILE%
+REM @echo OFF
 
-cd %HOME%
+setx HOME %USERPROFILE%
+set HOME=%USERPROFILE%
+
+pushd %HOME%
+
+:choice
+set /P DELETE_EXISTING=Do you want to delete existing files[Y/N]?
+if /I "%DELETE_EXISTING%" EQU "Y" goto :delete_existing_files
+if /I "%DELETE_EXISTING%" EQU "N" goto :create_links
+goto :choice
+
+:create_links
 
 mklink /H _vimrc .dotfiles\vim\.vimrc
 mklink /J .vim .dotfiles\vim\.vim
 
-mklink /H .minttyrc .dotfiles\mintty\.mintty
+mklink /H .minttyrc .dotfiles\mintty\.minttyrc
 mklink /J .mintty .dotfiles\mintty\.mintty
 
 mklink /H .gitconfig .dotfiles\git\.gitconfig
+mklink /H .gitconfig.os .dotfiles\git-win\.gitconfig.os
 mklink /H .gitignore_global .dotfiles\git\.gitignore_global
 mklink /H .gitmailmap .dotfiles\git\.gitmailmap
+
+popd
+
+goto :eof
+
+:delete_existing_files
+REM Hard links do not seem to work properly, so for now add option to remove file
+del _vimrc
+del .minttyrc
+del .gitconfig
+del .gitconfig.os
+del .gitignore_global
+del .gitmailmap
+
+goto :create_links
+
+:eof
+
+
